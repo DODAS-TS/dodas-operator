0356e6940b451c45a35e993c741a33bdad4542c4
dciangot <diego.ciangottini@gmail.com>
2020-01-22 09:54:49 +0100

    first commit

---
+++ pkg/apis/dodas/group.go
@@ -0,0 +1,6 @@
+// Package dodas contains dodas API versions.
+//
+// This file ensures Go source parsers acknowledge the dodas package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package dodas
---
+++ pkg/apis/dodas/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the dodas v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=dodas.infn.it
+package v1alpha1
